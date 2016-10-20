17e43878efc2ed50420f8a0d1608ece840d554be
Gregory M. Green <gregorymgreen@gmail.com>
2016-10-20 08:55:11 -0700

    Updated release number.

--- setup.py
+++ setup.py
@@ -113,11 +113,11 @@ def readme():
 
 setup(
     name='dustmaps',
-    version='0.1a6',
+    version='0.1a7',
     description='Uniform interface for multiple dust reddening maps.',
     long_description=readme(),
     url='https://github.com/gregreen/dustmaps',
-    download_url='https://github.com/gregreen/dustmaps/tarball/v0.1a6',
+    download_url='https://github.com/gregreen/dustmaps/tarball/v0.1a7',
     author='Gregory M. Green',
     author_email='gregorymgreen@gmail.com',
     license='GPLv2',
