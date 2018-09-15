e5ec6e87045a77cba0b2ac071d0ac2b154e0c149
Gregory M. Green <gregorymgreen@gmail.com>
2018-09-15 19:31:27 -0400

    Added Peek & Graves (2010). Refactored SFD code to separate out SFDBase, for all maps stored in same manner as SFD.
    See issue #13.

---
+++ dustmaps/pg2010.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+#
+# pg2010.py
+# Reads the Peek & Graves (2010) correction to the SFD'98 dust reddening map.
+#
+# Copyright (C) 2018  Gregory M. Green
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+from __future__ import print_function, division
+
+import os
+import numpy as np
+
+from .std_paths import *
+from .map_base import DustMap, WebDustMap, ensure_flat_galactic
+from . import fetch_utils
+from . import dustexceptions
+from .sfd import SFDBase
+
+
+class PG2010Query(SFDBase):
+    """
+    Queries the Peek & Graves (2010) correction to the SFD'98 dust reddening map.
+    """
+
+    map_name = 'pg2010'
+    map_name_long = "Peek & Graves (2010)"
+    poles = ['ngp']
+
+    def __init__(self, map_dir=None, component='dust'):
+        """
+        Args: map_dir (Optional[str]): The directory containing the SFD map.
+                Defaults to `None`, which means that `dustmaps` will look in its
+                default data directory.
+            component (Optional[str]): 'dust' (the default) to load the correction
+                to E(B-V), or 'err' to load the uncertainty in the correction.
+        """
+        
+        if map_dir is None:
+            map_dir = os.path.join(data_dir(), 'pg2010')
+        
+        if component not in ['dust', 'err']:
+            raise ValueError('`component` must be either "dust" or "err"')
+
+        base_fname = os.path.join(map_dir, 'PG_{}_4096'.format(component))
+        
+        super(PG2010Query, self).__init__(base_fname)
+    
+    def query(self, coords, order=1):
+        """
+        Returns the P&G (2010) correction to the SFD'98 E(B-V) at the specified
+        location(s) on the sky. If component is 'err', then return the
+        uncertainty in the correction.
+
+        Args:
+            coords (`astropy.coordinates.SkyCoord`): The coordinates to query.
+            order (Optional[int]): Interpolation order to use. Defaults to `1`,
+                for linear interpolation.
+
+        Returns:
+            A float array containing the P&G (2010) correction (or its
+            uncertainty) to SFD'98 at every input coordinate. The shape
+            of the output will be the same as the shape of the coordinates
+            stored by `coords`.
+        """
+        return super(PG2010Query, self).query(coords, order=order)
+    
+
+def fetch():
+    """
+    Downloads the Peek & Graves (2010) dust map, placing it in
+    the data directory for `dustmap`.
+    """
+    doi = '10.7910/DVN/VBSI4A'
+    
+    for component in ['dust', 'err']:
+        requirements = {'filename': 'PG_{}_4096_ngp.fits'.format(component)}
+        local_fname = os.path.join(
+            data_dir(),
+            'pg2010', 'PG_{}_4096_ngp.fits'.format(component))
+        print('Downloading P&G (2010) {} data file to {}'.format(
+            component, local_fname))
+        fetch_utils.dataverse_download_doi(
+            doi,
+            local_fname,
+            file_requirements=requirements)
--- dustmaps/sfd.py
+++ dustmaps/sfd.py
@@ -3,7 +3,7 @@
 # sfd.py
 # Reads the Schlegel, Finkbeiner & Davis (1998; SFD) dust reddening map.
 #
-# Copyright (C) 2016  Gregory M. Green, Edward F. Schlafly
+# Copyright (C) 2016-2018  Gregory M. Green, Edward F. Schlafly
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -35,10 +35,69 @@ from . import fetch_utils
 from . import dustexceptions
 
 
-class SFDQuery(DustMap):
+class SFDBase(DustMap):
     """
-    Queries the Schlegel, Finbeiner & Davis (1998) dust reddening map.
+    Queries maps stored in the same format as Schlegel, Finkbeiner & Davis (1998).
     """
+
+    map_name = ''
+    map_name_long = ''
+    poles = ['ngp', 'sgp']
+
+    def __init__(self, base_fname):
+        """
+        Args:
+            base_fname (str): The map should be stored in two FITS files, named
+                ``base_fname + '_' + X + '.fits'``, where ``X`` is ``'ngp'`` and
+                ``'sgp'``.
+        """
+        self._data = {}
+
+        for pole in self.poles:
+            fname = '{}_{}.fits'.format(base_fname, pole)
+            try:
+                with fits.open(fname) as hdulist:
+                    self._data[pole] = [hdulist[0].data, wcs.WCS(hdulist[0].header)]
+            except IOError as error:
+                print(dustexceptions.data_missing_message(self.map_name,
+                                                          self.map_name_long))
+                raise error
+
+    @ensure_flat_galactic
+    def query(self, coords, order=1):
+        """
+        Returns the map value at the specified location(s) on the sky.
+
+        Args:
+            coords (`astropy.coordinates.SkyCoord`): The coordinates to query.
+            order (Optional[int]): Interpolation order to use. Defaults to `1`,
+                for linear interpolation.
+
+        Returns:
+            A float array containing the map value at every input coordinate.
+            The shape of the output will be the same as the shape of the
+            coordinates stored by `coords`.
+        """
+        out = np.full(len(coords.l.deg), np.nan, dtype='f4')
+
+        for pole in self.poles:
+            m = (coords.b.deg >= 0) if pole == 'ngp' else (coords.b.deg < 0)
+
+            if np.any(m):
+                data, w = self._data[pole]
+                x, y = w.wcs_world2pix(coords.l.deg[m], coords.b.deg[m], 0)
+                out[m] = map_coordinates(data, [y, x], order=order, mode='nearest')
+
+        return out
+
+
+class SFDQuery(SFDBase):
+    """
+    Queries the Schlegel, Finkbeiner & Davis (1998) dust reddening map.
+    """
+
+    map_name = 'sfd'
+    map_name_long = "SFD'98"
 
     def __init__(self, map_dir=None):
         """
@@ -47,25 +106,14 @@ class SFDQuery(DustMap):
                 Defaults to `None`, which means that `dustmaps` will look in its
                 default data directory.
         """
-
+        
         if map_dir is None:
             map_dir = os.path.join(data_dir(), 'sfd')
 
-        self._data = {}
-
         base_fname = os.path.join(map_dir, 'SFD_dust_4096')
-
-        for pole in ['ngp', 'sgp']:
-            fname = '{}_{}.fits'.format(base_fname, pole)
-            try:
-                with fits.open(fname) as hdulist:
-                    self._data[pole] = [hdulist[0].data, wcs.WCS(hdulist[0].header)]
-            except IOError as error:
-                print(dustexceptions.data_missing_message('sfd',
-                                                          "SFD'98"))
-                raise error
-
-    @ensure_flat_galactic
+        
+        super(SFDQuery, self).__init__(base_fname)
+    
     def query(self, coords, order=1):
         """
         Returns E(B-V) at the specified location(s) on the sky. See Table 6 of
@@ -82,18 +130,8 @@ class SFDQuery(DustMap):
             The shape of the output will be the same as the shape of the
             coordinates stored by `coords`.
         """
-        out = np.zeros(len(coords.l.deg), dtype='f4')
-
-        for pole in ['ngp', 'sgp']:
-            m = (coords.b.deg >= 0) if pole == 'ngp' else (coords.b.deg < 0)
-
-            if np.any(m):
-                data, w = self._data[pole]
-                x, y = w.wcs_world2pix(coords.l.deg[m], coords.b.deg[m], 0)
-                out[m] = map_coordinates(data, [y, x], order=order, mode='nearest')
-
-        return out
-
+        return super(SFDQuery, self).query(coords, order=order)
+    
 
 class SFDWebQuery(WebDustMap):
     """
