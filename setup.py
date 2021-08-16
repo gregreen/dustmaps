71f0f88422ec7d4a8dc6321961b7c826fa48eaf2
Gregory M. Green <gregorymgreen@gmail.com>
2021-08-16 15:19:17 +0200

    Bumped minor version number.

--- setup.py
+++ setup.py
@@ -153,12 +153,12 @@ def readme():
 
 setup(
     name='dustmaps',
-    version='1.0.8',
+    version='1.0.9',
     description='Uniform interface for multiple dust reddening maps.',
     long_description=readme(),
     long_description_content_type='text/markdown',
     url='https://github.com/gregreen/dustmaps',
-    download_url='https://github.com/gregreen/dustmaps/archive/v1.0.8.tar.gz',
+    download_url='https://github.com/gregreen/dustmaps/archive/v1.0.9.tar.gz',
     author='Gregory M. Green',
     author_email='gregorymgreen@gmail.com',
     license='GPLv2',
