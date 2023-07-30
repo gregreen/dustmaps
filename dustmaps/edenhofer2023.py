f92e0fbea469f6d6e7d4a460e1e5fd2b4c5c1ad0
Gregory M. Green <gregorymgreen@gmail.com>
2023-07-30 17:27:00 +0200

    Added GPLv2 header to edenhofer2023.py.

--- dustmaps/edenhofer2023.py
+++ dustmaps/edenhofer2023.py
@@ -1,4 +1,25 @@
-# Copyright(C) 2023 Gordian Edenhofer
+#!/usr/bin/env python
+#
+# edenhofer2023.py
+# Reads the Edenhofer2023 dust map, which is described in
+# Edenhofer et al. (2023).
+#
+# Copyright (C) 2023 Gordian Edenhofer
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
 
 from __future__ import division, print_function
 
