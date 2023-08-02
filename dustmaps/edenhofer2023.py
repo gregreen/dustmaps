d6284782a08cbe1f4483077ab093e5e784b30b2e
Gregory M. Green <gregorymgreen@gmail.com>
2023-08-02 15:07:58 +0200

    Update copyright strings

--- dustmaps/edenhofer2023.py
+++ dustmaps/edenhofer2023.py
@@ -4,7 +4,7 @@
 # Reads the Edenhofer2023 dust map, which is described in
 # Edenhofer et al. (2023).
 #
-# Copyright (C) 2023 Gordian Edenhofer
+# Copyright (C) 2023  Gordian Edenhofer and Gregory M. Green
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- dustmaps/tests/test_edenhofer2023.py
+++ dustmaps/tests/test_edenhofer2023.py
@@ -1,4 +1,24 @@
-# Copyright(C) 2023 Gordian Edenhofer
+#!/usr/bin/env python
+#
+# test_edenhofer2023.py
+# Tests the query code for the Edenhofer2023 dust map (Edenhofer et al. 2023).
+#
+# Copyright (C) 2023  Gordian Edenhofer and Gregory M. Green
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
 
 from __future__ import division, print_function
 
