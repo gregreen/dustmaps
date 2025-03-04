a6609dbcf709a71076390f193b61cbc1003a9590
Gregory M. Green <gregorymgreen@gmail.com>
2025-03-04 17:49:35 +0800

    Switching to built-in Sphinx napoleon.

--- docs/conf.py
+++ docs/conf.py
@@ -37,7 +37,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.napoleon',
+    'sphinx.ext.napoleon',
     # 'sphinxcontrib.googleanalytics'
     # 'sphinxcontrib.programoutput'
 ]
